9a53e0de06e5d69a36e1fa309712875d7b44fd68
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-06-29 23:50:51 -0300

    Esqueleto do projeto

---
+++ setup.py
@@ -0,0 +1,24 @@
+i# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.md') as f:
+    license = f.read()
+
+setup(
+    name='WebProxyCache',
+    version='0.1.0',
+    description='Trablho para a classe de Redes de Computadores 1, 2018/1 - INF/UFG',
+    long_description=readme,
+    author='Leonardo M Fleury',
+    author_email='fleuryleomoraes@gmailcom',
+    url='https://github.com/leuzera/WebProxyCache',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
