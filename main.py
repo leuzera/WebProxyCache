26ef4c21847c4ec00dd7701e6f6c6335b81279ba
Leonardo Moraes Fleury <fleuryleomoraes@gmail.com>
2018-07-12 19:19:51 -0300

    Logger

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import proxycache
+import logging
+from logging.config import fileConfig
+
+fileConfig('logging_config.ini')
+logger = logging.getLogger()
 
 proxy = proxycache.ProxyCache()
 proxy.run()
